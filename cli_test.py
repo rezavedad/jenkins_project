8b8c923975aaaf010542a224475dd438a226e4d9
Reza Vedad <101221348+rezavedad@users.noreply.github.com>
2022-11-05 22:36:08 +0100

    Add files via upload

---
+++ cli_test.py
@@ -0,0 +1,8 @@
+# C:\Users\Rez\PycharmProjects\advanced\Diverse>python cli_test.py
+
+# C:\Users\Rez\PycharmProjects\advanced\Diverse>python cli_test.py
+# Hi, What is your name? rez
+# Hello rez
+
+name = input('Hi, What is your name? ')
+print(f'Hello {name}')
